8c569b0c3f1724ac5a91bef9101a75a3683e241c
ajeyajaz <ajeyajaz@gmail.com>
2026-01-08 16:51:07 +0530

    define the grid layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,16 @@
 function App() {
 
   return (
-    <>
-        <h1>hello world!</h1>
-    </>
+    <div className="grid border lg: grid-cols-5">
+      {/* nav */}
+      <div className="col-span-full bg-amber-500">nav</div>
+      {/* sideBar */}
+      <div className="hidden bg-amber-100 lg:block">
+        sidebar
+      </div>
+      {/* main */}
+      <div className="col-span-full bg-amber-300 lg:col-start-2">main</div>
+    </div>
   )
 }
 
