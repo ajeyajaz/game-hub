5fcd021ecd84508fb124a773f7a0c841489aa4fc
ajeyajaz <ajeyajaz@gmail.com>
2026-01-12 09:41:56 +0530

    cleanup the gameGrid

--- src/components/GameGrid.jsx
+++ src/components/GameGrid.jsx
@@ -7,19 +7,19 @@ function GameGrid({ gameQuery }) {
     const { data: games, error, isLoading } = useGames(gameQuery);
     const skeletons = [1, 2, 3, 4, 5, 6];
 
+    if(error) return <p className="text-red-500">{error}</p>
 
     return (
         <>
-            {error && <p>{error}</p>}
             <div
-                className="grid grid-cols-1 gap-4 justify-items-center items-center px-2 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-5"
+                className="grid grid-cols-1 gap-4 justify-items-center items-center px-2 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4"
             >
                 {isLoading && skeletons.map(skeleton => (
                     <GameCardContainer key={skeleton}>
                         <GameCardSkeleton />
                     </GameCardContainer>)
                 )}
-                {games.map(game => (
+                {!isLoading && games.map(game => (
                     <GameCardContainer key={game.id} >
                         <GameCard game={game} />
                     </GameCardContainer>)
