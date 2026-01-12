e3a0d3b1f01412afb411e4b4fe3c4ca1bc36925e
ajeyajaz <ajeyajaz@gmail.com>
2026-01-12 07:43:10 +0530

    display the search box

---
+++ src/components/InputSearch.jsx
@@ -0,0 +1,8 @@
+
+function InputSearch({onSearch, className=''}) {
+  return (
+    <input type="text" className={className} />
+  )
+}
+
+export default InputSearch
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,14 @@
 import logo from '../assets/logo.webp'
+import InputSearch from './InputSearch'
 
 function NavBar() {
     return (
         <>
-            <nav className="flex">
+            <nav className="flex items-center gap-2 pr-2 mb-4">
                 <div className='w-12 h-12'>
                     <img src={logo} alt="/" className='w-full h-full object-contain' />
                 </div>
-                <p>NavBar</p>
+                <InputSearch className='border border-gray-400 w-full max-w-xl mx-auto rounded-xl px-2 py-0.5' />
             </nav>
         </>
     )
