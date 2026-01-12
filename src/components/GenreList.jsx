51747d07d07b9c6b6922581c3a1968d2891b7ac9
ajeyajaz <ajeyajaz@gmail.com>
2026-01-12 08:59:46 +0530

    cleanup the genre

--- src/components/GenreList.jsx
+++ src/components/GenreList.jsx
@@ -3,27 +3,32 @@ import imageUrl from "../services/image-url";
 import Spinner from "./Spinner";
 
 function GenreList({ setGameQuery, selectedGenre }) {
-    const { data: genres, error, isLoading } = useGenres(); 
+    const { data: genres, error, isLoading } = useGenres();
 
     if (error) return null;
     if (isLoading) return <Spinner />;
 
+    const highlight = 'text-white font-semibold transition-all duration-300'
+
     return (
-        <ul className="text-[#a1a1a1]">
-            {genres.map(g => (
-                <li key={g.id} className="flex items-center gap-2 py-1">
-                    <div className="w-10 h-10 rounded-lg overflow-hidden">
-                        <img src={imageUrl(g.image_background)} alt="" className="w-full h-full object-cover" />
-                    </div>
-                    <button 
-                        className={selectedGenre?.id === g.id ? ' text-white font-semibold transition-all duration-300' : ''}
-                        onClick={() => setGameQuery(g)}
-                    >
-                        {g.name}
-                    </button>
-                </li>
-            ))}
-        </ul>
+        <>
+            <h2 className="text-2xl font-bold mb-4">Genre</h2>
+            <ul className="text-[#a1a1a1]">
+                {genres.map(g => (
+                    <li key={g.id} className="flex items-center gap-2 py-1">
+                        <div className="w-10 h-10 rounded-lg overflow-hidden">
+                            <img src={imageUrl(g.image_background)} alt="" className="w-full h-full object-cover" />
+                        </div>
+                        <button
+                            className={`text-left ${selectedGenre?.id === g.id ? highlight : ''}`}
+                            onClick={() => setGameQuery(g)}
+                        >
+                            {g.name}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </>
     )
 }
 
